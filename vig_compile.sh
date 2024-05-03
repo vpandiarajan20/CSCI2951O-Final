d84d56c306d61a53e7a0ca2b2bed85351ddac962
Vignesh <vpandiarajan20@gmail.com>
2024-05-03 00:32:39 -0400

    delete unneccesary files

--- vig_compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-########################################
-############# CSCI 2951-O ##############
-########################################
-
-# Update this file with instructions on how to compile your code
-javac ./src/solver/ls/*.java
