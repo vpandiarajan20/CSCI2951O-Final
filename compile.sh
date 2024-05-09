14389c46077a3bd28ca5b3cedb13d13558c1f618
vpandiarajan20 <vpandiarajan20@gmail.com>
2024-05-09 14:59:08 -0400

    test push

--- compile.sh
+++ compile.sh
@@ -6,3 +6,5 @@
 
 # Update this file with instructions on how to compile your code
 javac ./src/solver/ls/*.java ./src/solver/ls/insertionHeuristics/*.java ./src/solver/ls/removalHeuristics/*.java
+
+#hey
